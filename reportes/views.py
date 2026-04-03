ff68f2b374dba490aee2c873be0051d6e1f379f9
Juan Caldas <jc.caldas23@uniandes.edu.co>
2026-04-03 18:48:15 -0500

    Fix reportes view - GET endpoint optimizado

--- reportes/views.py
+++ reportes/views.py
@@ -2,27 +2,21 @@ import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from reportes.logic.logic_reportes import get_reporte_mensual, get_total_por_proveedor
-from django.core import serializers
 
 @csrf_exempt
 def reporte_mensual(request):
-    if request.method == 'POST':
-        body = json.loads(request.body)
-        empresa_id = body.get('empresa_id')
-        mes = body.get('mes')
-        anio = body.get('anio')
+    if request.method == 'GET':
+        empresa_id = request.GET.get('empresa_id', 1)
+        mes = request.GET.get('mes', 3)
+        anio = request.GET.get('anio', 2026)
 
-        consumos = get_reporte_mensual(empresa_id, mes, anio)
         totales = get_total_por_proveedor(empresa_id, mes, anio)
-
-        consumos_dto = serializers.serialize('json', consumos)
 
         response = {
             'empresa_id': empresa_id,
             'mes': mes,
             'anio': anio,
-            'total_por_proveedor': totales,
-            'detalle': json.loads(consumos_dto)
+            'total_por_proveedor': totales
         }
         return HttpResponse(json.dumps(response), content_type='application/json')
 
