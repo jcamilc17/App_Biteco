62d2dd359e1e5e8194606484dd1ae334f587b045
Juan Caldas <jc.caldas23@uniandes.edu.co>
2026-04-04 09:38:21 -0500

    Correccion Health Check

--- bitecoapp/urls.py
+++ bitecoapp/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
